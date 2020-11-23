634de83784c6df9b904573632b15bf20b0a64f6d
saantiago-sg <54604815+saantiago-sg@users.noreply.github.com>
2020-11-23 19:25:08 -0300

    Se ajusto el filtro de desktop, resta hacer el de mobile

--- js/biblioteca.js
+++ js/biblioteca.js
@@ -1,3 +1,4 @@
+// --- DESKTOP ---
 let btnFiltrar = document.querySelector("#btnFiltrar").addEventListener("click", filtrar);
 
 function filtrar(){
@@ -7,5 +8,18 @@ function filtrar(){
         document.querySelector("#imgFiltro").src = "../img/filtrar_cel.png";
     }else{
         document.querySelector("#imgFiltro").src = "../img/filtrar_gris.png";
+    }
+}
+
+// --- MOBILE ---
+let btnFiltrarMob = document.querySelector("#btnFiltrarMob").addEventListener("click", filtrarMob);
+
+function filtrarMob(){
+    let mostrarFiltroMob = document.querySelector("#filtroMob").classList.toggle("filtro-bl")
+
+    if(mostrarFiltroMob){
+        document.querySelector("#imgFiltroMob").src = "../img/filtrar_cel.png";
+    }else{
+        document.querySelector("#imgFiltroMob").src = "../img/filtrar_gris.png";
     }
 }

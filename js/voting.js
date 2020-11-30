99df99e49504d08c8089b0c7ce48240313479563
saantiago-sg <54604815+saantiago-sg@users.noreply.github.com>
2020-11-30 02:20:37 -0300

    segunda parte de efects

--- js/voting.js
+++ js/voting.js
@@ -35,9 +35,32 @@ stars.forEach((star, i) => {
 })
 
 vote.addEventListener('click', () => {
+<<<<<<< Updated upstream
     voteHelpText.innerText = 'Gracias por tu calificacion!'
     commentsContainer.classList.add("hide")
 })
 
 
 let a = 1
+=======
+    vote.classList.add("load");
+    let time = setTimeout(function(){
+        voteHelpText.innerText = 'Gracias por tu calificacion!'
+        commentsContainer.classList.add("hide")
+        let content = commentContent.value
+        document.cookie = `content=${content}`
+        document.cookie = `stars=${voteStars}`
+        console.log(document.cookie)
+    }, 2500);
+})
+
+// vote.addEventListener('click', () => {
+//     voteHelpText.innerText = 'Gracias por tu calificacion!'
+//     commentsContainer.classList.add("hide")
+//     let content = commentContent.value
+//     document.cookie = `content=${content}`
+//     document.cookie = `stars=${voteStars}`
+//     console.log(document.cookie)
+// })
+
+>>>>>>> Stashed changes
